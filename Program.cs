129209396b4509dda5364f7df2552c04c9f1ba69
Alex Momotyuk <o.a.momotyuk@gmail.com>
2019-11-18 12:14:19 -0800

    Planning of heist phase two is done

--- Program.cs
+++ Program.cs
@@ -8,25 +8,45 @@ namespace __11._16_Plan_Your_Heist
         static void Main(string[] args)
         {
             /*
-            // Phase 1
+            // Phase 1, phase 2
             */
 
             // 1. Print the message "Plan Your Heist!".
             Console.WriteLine("Plan Your Heist!");
-
-            // 2. Create a way to store a single team member. A team member will have a name, a skill Level and a courage factor. The skill Level will be a positive integer and the courage factor will be a decimal between 0.0 and 2.0.
-            Dictionary<string, string> teamMember = new Dictionary<string, string>();
-
-            teamMember = getTeamMember();
             Console.Write("\n");
 
-            // 6. Display the team member's information.
-            if( teamMember.ContainsKey("Name") )
+            // 1-2. Create a way to store a single team member. A team member will have a name, a skill Level and a courage factor. The skill Level will be a positive integer and the courage factor will be a decimal between 0.0 and 2.0.
+            Dictionary<string, string> teamMember = new Dictionary<string, string>();
+
+            // 2-1. Create a way to store several team members
+            List<Dictionary<string, string>> teamMembers = new List<Dictionary<string, string>>();
+
+            // 2-2. Collect several team members' information
+            do{
+                if( teamMember.ContainsKey("Name") )
+                {
+                    teamMembers.Add( teamMember );
+                }
+                teamMember = getTeamMember();
+            }
+            while( teamMember["Name"] != "" ); // 2-3. Stop collecting team members when a blank name is entered
+
+            Console.Write("\n");
+
+            // 2-4. Display a message containing the number of members of the team
+            Console.WriteLine($"Number of team members: {teamMembers.Count}");
+
+            Console.Write("\n");
+
+            // 2-5. Display each team member's information
+            foreach( Dictionary<string, string> member in teamMembers )
             {
-                foreach (KeyValuePair<string, string> attribute in teamMember )
+                // 1-6. Display the team member's information.
+                foreach (KeyValuePair<string, string> attribute in member )
                 {
                     Console.WriteLine($"{attribute.Key}: {attribute.Value}");
                 }
+                Console.Write("\n");
             }
         }
 
@@ -34,20 +54,23 @@ namespace __11._16_Plan_Your_Heist
         {
             Dictionary<string, string> teamMember = new Dictionary<string, string>();
 
-            // 3. Prompt the user to enter a team member's name and save that name.
-            Console.WriteLine("Enter your team member's name");
+            // 1-3. Prompt the user to enter a team member's name and save that name.
+            Console.WriteLine("Enter your team member's name:");
             string name = Console.ReadLine();
             teamMember.Add("Name", name);
 
-            // 4. Prompt the user to enter a team member's skill level and save that skill level with the name.
-            Console.WriteLine("Enter your team member's skill level");
-            string skillLevel = Console.ReadLine();
-            teamMember.Add("Skill Level", skillLevel);
+            if( teamMember["Name"] != "" )
+            {
+                // 1-4. Prompt the user to enter a team member's skill level and save that skill level with the name.
+                Console.WriteLine("Enter your team member's skill level:");
+                string skillLevel = Console.ReadLine();
+                teamMember.Add("Skill Level", skillLevel);
 
-            // 5. Prompt the user to enter a team member's courage factor and save that courage factor with the name.
-            Console.WriteLine("Enter your team member's courage factor");
-            string courageFactor = Console.ReadLine();
-            teamMember.Add("Courage Factor", courageFactor);
+                // 1-5. Prompt the user to enter a team member's courage factor and save that courage factor with the name.
+                Console.WriteLine("Enter your team member's courage factor:");
+                string courageFactor = Console.ReadLine();
+                teamMember.Add("Courage Factor", courageFactor);
+            }
 
             return teamMember;
         }
