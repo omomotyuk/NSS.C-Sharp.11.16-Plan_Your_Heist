e69bcbaa9d960c9fb51ed8b550ad5e8fc8d68aef
Alex Momotyuk <o.a.momotyuk@gmail.com>
2019-11-18 09:39:57 -0800

    Planning of heist phase five is done

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace __11._16_Plan_Your_Heist
         static void Main(string[] args)
         {
             /*
-            // Phase 1, phase 2, phase 3
+            // Phase 1, phase 2, phase 3, phase 4, phase 5
             */
 
             // 1. Print the message "Plan Your Heist!".
@@ -30,10 +30,13 @@ namespace __11._16_Plan_Your_Heist
             }
             while( teamMember["Name"] != "" ); // 2-3. Stop collecting team members when a blank name is entered
 
-            Console.Write("\n");
-
             // 2-4. Display a message containing the number of members of the team
-            Console.WriteLine($"Number of team members: {teamMembers.Count}\n");
+            Console.WriteLine($"Number of team members: {teamMembers.Count}");
+
+            // 5-2. After the user enters the team information, prompt them to enter the number of trial runs the program should perform
+            Console.Write("Enter number of trial runs: ");
+            int trialRunNumbers = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n");
 
             // 3-1. Stop displaying each team member's information
             //showTeamInfo(teamMembers);
@@ -47,23 +50,31 @@ namespace __11._16_Plan_Your_Heist
             // 4-1. Create a random number between -10 and 10 for the heist's luck value
             Random heistLuckValue = new Random();
 
-            // 4-2. Add this number to the bank's difficulty level
-            bankDifficultyLevel += heistLuckValue.Next(-10,10);
-
-            // 4-3. Before displaying the success or failure message, display a report that shows
-            // 4-3. The bank's difficulty level
-            Console.WriteLine($"Bank's difficulty level: {bankDifficultyLevel}");
-            // 4-3. The team's combined skill level
-            Console.WriteLine($"Teams skill level sum: {teamSkillLevel}");
-
-            // 3-4. Compare the number with the bank's difficulty level. If the team's skill level is greater than or equal to the bank's difficulty level, Display a success message, otherwise display a failure message.
-            if( teamSkillLevel >= bankDifficultyLevel )
+            // 5-1. Run the scenario multiple times
+            for( int i = 0; i < trialRunNumbers; i++ )
             {
-                Console.WriteLine("Bank heist - success");
-            }
-            else
-            {
-                Console.WriteLine("Bank heist - failure");
+                int currentBankDLevel = bankDifficultyLevel;
+
+                Console.WriteLine($"Trial #{i+1}");
+                // 4-2. Add this number to the bank's difficulty level
+                // 5-3. Loop through the difficulty / skill level calculation based on the user-entered number of trial runs. Choose a new luck value each time
+                currentBankDLevel += heistLuckValue.Next(-10,10);
+
+                // 4-3. Before displaying the success or failure message, display a report that shows
+                // 4-3. The bank's difficulty level
+                Console.WriteLine($"Bank's difficulty level: {currentBankDLevel}");
+                // 4-3. The team's combined skill level
+                Console.WriteLine($"Teams skill level sum: {teamSkillLevel}");
+
+                // 3-4. Compare the number with the bank's difficulty level. If the team's skill level is greater than or equal to the bank's difficulty level, Display a success message, otherwise display a failure message.
+                if( teamSkillLevel >= currentBankDLevel )
+                {
+                    Console.WriteLine("Bank heist - success\n");
+                }
+                else
+                {
+                    Console.WriteLine("Bank heist - failure\n");
+                }
             }
 
         }
@@ -89,6 +100,8 @@ namespace __11._16_Plan_Your_Heist
                 string courageFactor = Console.ReadLine();
                 teamMember.Add("Courage Factor", courageFactor);
             }
+
+            Console.Write("\n");
 
             return teamMember;
         }
